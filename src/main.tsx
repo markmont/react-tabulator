8e72509ae37a911c727fc758ec1c3c1c18af1848
Mark Montague <markmont@umich.edu>
2025-12-24 09:38:18 -0500

    fix more typescript errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
-export { default as ReactTabulator, ReactTabulatorOptions, ColumnDefinition } from './ReactTabulator';
+export { default as ReactTabulator } from './ReactTabulator';
+export type { ReactTabulatorOptions, ColumnDefinition } from './ReactTabulator';
 export { default as React15Tabulator } from './React15Tabulator';
 export { default as ReactTabulatorExample } from './ReactTabulatorExample';
 export { reactFormatter } from './Utils';
